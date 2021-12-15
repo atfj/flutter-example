c6970d3a065db9163423ff3cc820ced4ecb9daa5
atfj <a9fujita@gmail.com>
2021-12-16 02:39:23 +0900

    Implement main detailed view

--- lib/pages/pokemon_detail.dart
+++ lib/pages/pokemon_detail.dart
@@ -1,6 +1,12 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// Project imports:
+import 'package:flutter_example/data/poke_resources_provider.dart';
+
 class PokemonDetail extends StatelessWidget {
   const PokemonDetail({
     Key? key,
@@ -15,14 +21,12 @@ class PokemonDetail extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter Example'),
       ),
-      body: Center(
-        child: _PokemonDetail(name: name,)
-      )
+      body: _PokemonDetail(name: name,)
     );
   }
 }
 
-class _PokemonDetail extends StatelessWidget {
+class _PokemonDetail extends ConsumerWidget {
   const _PokemonDetail({
     Key? key,
     required this.name
@@ -31,7 +35,22 @@ class _PokemonDetail extends StatelessWidget {
   final String name;
 
   @override
-  Widget build(BuildContext context) {
-    return Text(name);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(pokemonProvider(name));
+    
+    return state.when(
+      data: (poke) => Center(child: 
+        Column(children: [
+          const SizedBox(height: 32,),
+          poke.sprites.frontDefault != null ?
+              Image.network(poke.sprites.frontDefault!) :
+              const Text('No Image'),
+          const SizedBox(height: 16,),
+          Text(name)
+        ],),
+      ),
+      error: (e, _) => const Center(child: Text('Error'),), 
+      loading: () => const Center(child: CircularProgressIndicator(),)
+    );
   }
 }
